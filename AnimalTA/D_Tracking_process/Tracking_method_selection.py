0f8d7c80b70a8b16307265a8da34b274d87bbc34
Michael Perkinson <michael.perkinson95@gmail.com>
2026-05-07 16:05:55 +1200

    Replace tracking method benchmark with CPU affinity check
    The benchmark ran a timing test before every track, causing long hangs
    on HPC. Replace with a direct check of allocated CPUs via sched_getaffinity
    (falls back to cpu_count on Windows). Multi is used when allocated CPUs >= 2
    and video duration >= 2000s, single otherwise.

--- AnimalTA/D_Tracking_process/Tracking_method_selection.py
+++ AnimalTA/D_Tracking_process/Tracking_method_selection.py
@@ -1,7 +1,5 @@
-import time
 from AnimalTA.D_Tracking_process import Do_the_track_multi, Do_the_track
 import multiprocessing
-import cv2
 import os
 import sys
 import datetime as _dt
@@ -13,28 +11,11 @@ def _tlog(msg):
     print(f"[track {ts}] {msg}", file=sys.stderr, flush=True)
 
 
-#We determine whether it is better to use multiprocessing method or not:
-def _estimate_opencv_capture_time(video_path, one_every):
-    capture = cv2.VideoCapture(video_path)
-    if not capture.isOpened():
-        return None
-
-    start_time = time.time()
-    grabbed_index = -1
-    for frame_index in range(0, int(one_every * 5) + 1, int(one_every)):
-        while grabbed_index < frame_index:
-            if not capture.grab():
-                capture.release()
-                return None
-            grabbed_index += 1
-        ret, frame = capture.retrieve()
-        if not ret or frame is None:
-            capture.release()
-            return None
-
-    elapsed = time.time() - start_time
-    capture.release()
-    return elapsed
+def _allocated_cpus():
+    try:
+        return len(os.sched_getaffinity(0))
+    except AttributeError:
+        return multiprocessing.cpu_count()
 
 
 def Choose_method(parent, Vid, folder, type, head_tail):
@@ -47,34 +28,17 @@ def Choose_method(parent, Vid, folder, type, head_tail):
         Low_Priority = Params["Low_priority"]
 
     duration=(Vid.Cropped[1][1]-Vid.Cropped[1][0])/(Vid.Frame_rate[0] / Vid.Frame_rate[1])
+    allocated = _allocated_cpus()
 
-    if duration < 2000 or Vid.Back[0] == 2 or Low_Priority:
+    if Low_Priority or Vid.Back[0] == 2 or duration < 2000 or allocated < 2:
         method=0
     else:
-        res_normal=Do_the_track.Do_tracking(parent, Vid, folder, type, portion=False, prev_row=None, arena_interest=None, test=True)
+        method=1
 
-        parent.timer = 0.0001
-        parent.show_load()
-
-        one_every = Vid.Frame_rate[0] / Vid.Frame_rate[1]
-
-        if Vid.type!="Video":
-            method=1
-        else:
-            res_multi = _estimate_opencv_capture_time(Vid.Fusion[0][1], one_every)
-
-            parent.timer = 0.0002
-            parent.show_load()
-
-            if res_multi is not None and (res_normal/5)*duration > (res_multi/5)*duration+20:
-                method=1
-            else:
-                method=0
-
-    if Low_Priority:  # Video beginning (after crop)
+    if Low_Priority:
         method=0
 
-    _tlog("tracking: method={} ({})".format("multi" if method else "single", Vid.User_Name))
+    _tlog("tracking: method={} (allocated_cpus={}, duration={:.0f}s, {})".format("multi" if method else "single", allocated, duration, Vid.User_Name))
     if method==0:
         succeed = Do_the_track.Do_tracking(parent=parent, Vid=Vid, type=type, folder=folder, test=False, head_tail=head_tail)
         return succeed
