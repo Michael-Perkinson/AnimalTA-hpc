fe3a6a9c64590f3e9b411d2febb4f4f92715df09
Michael Perkinson <michael.perkinson95@gmail.com>
2026-05-18 15:22:09 +1200

    Remove GPU probe allocation that false-negatives on memory pressure

--- AnimalTA/A_General_tools/gpu_utils.py
+++ AnimalTA/A_General_tools/gpu_utils.py
@@ -1,6 +1,5 @@
 try:
     import cupy as cp
-    cp.array([1], dtype=cp.uint32).astype(cp.float32)  # forces NVRTC JIT compile
     CUPY_AVAILABLE = True
     GPU_FALLBACK_REASON = None
 except Exception as e:
